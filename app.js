9edbce27c64cfd428255d4d07138a1776d24f378
Pavel Martakov <martackovp@yandex.ru>
2021-07-02 23:47:11 +0300

    Скрипт готов на 95%

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const puppeteer = require('puppeteer')
 const brEnsPoint = process.argv[2]
 
+const answers = [
+    'It is its own layer 2 blockchain',
+    'Option 1: 30,000; Option 2: 2,250',
+    'Users in the waiting room will be given a random spot in the queue when the sale starts. Users who arrive after the sale starts will be placed behind those in the waiting room',
+    'Option 1: Tokens unlock on or around September 8, 2021. Option 2: Tokens release linearly over 12 months starting from January 8, 2022',
+    'BTC, ETH, USDC, USDT',
+    'Option 1: $0.50 per token, $500 limit. Option 2: $0.225 per token, $1,000 limit',
+    'The user\'s purchase may be cancelled and the user may be banned from future CoinList sales',
+    'CoinList.co',
+    'coinlistofficialchannel',
+    'The user\'s account will be terminated and all purchases will be cancelled'
+]
+
 const start = async () => {
     const browser = await puppeteer.connect( {browserWSEndpoint: brEnsPoint} )
-    console.log('create browser');
+    console.log('create browser')
 
-    console.log('new page');
+    console.log('new page') 
     const page = await browser.newPage()
 
     page.setViewport({
@@ -13,76 +26,84 @@ const start = async () => {
         height: 1080
     })
 
-    // console.log('cross to dashboard');
-    // await Promise.all([
-    //     page.goto('https://coinlist.co/dashboard'),
-    //     page.waitForNavigation({ waitUntil: 'networkidle0' }),
-    //     page.waitForNavigation({ timeout: 10000 }),
-    // ]);
-
-    
-
-    
-
-    console.log('cross to QREDO');
+    console.log('cross to QREDO') 
     await Promise.all([
         page.goto('https://coinlist.co/qredo'),
         page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
-    ]);
+    ]) 
 
-    console.log('click REGISTER');
+    console.log('click REGISTER') 
     await Promise.all([
         page.click('#cover_top_content > div > div > div.s-grid.s-marginTop4.s-grid--bottom.u-textAlignCenter > div.index-deal_page_index-header__cta_section.s-grid--bottom.s-grid-colMd8.u-textAlignLeft > div > a'),
         page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
-    ]);
+    ]) 
 
-    console.log('click REGISTER NOW');
+    console.log('click REGISTER NOW') 
     await Promise.all([
         await page.click('#token-sale > div > div > div > div > div > div.s-grid.u-text-center.s-marginTop2 > div:nth-child(1) > div > div > a'),
-        page.waitForTimeout(2000)
-    ]); 
+        page.waitForTimeout(1000)
+    ])  
     
-    console.log('cross to login');
+    console.log('cross to login')
+    console.log('Waiting for 7s...'); 
     await Promise.all([
         page.click('#new_user > div > div.s-gridC-colSm24.u-text-center > a'),
-        page.waitForTimeout(10000)
-    ]); 
+        page.waitForTimeout(7000)
+    ])  
+    
+    // console.log('input DATA')     
+    // await page.type('#user_email', 'plyushchev-danila@mail.ru')
+    // await page.type('#user_password', 'oR6ZrQ6MPfA')
 
-    console.log('input DATA');    
-    await page.type('#user_email', 'plyushchev-danila@mail.ru')
-    await page.type('#user_password', 'oR6ZrQ6MPfA')
-
-    console.log('click LOGIN');
+    console.log('click LOGIN') 
     await Promise.all([
         page.click('#new_user > div > div:nth-child(4) > input'),
         page.waitForNavigation({ waitUntil: 'domcontentloaded' })
     ])
 
-    console.log('click GET STARTED');
-    await Promise.all([
-        await page.click('body > div.s-container.s-gridMaxXs24.layouts-shared-offering_flow > div > div > div.s-grid-colSm16 > div > div > a'),
-        page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
-    ]); 
+    console.log(`
+    ****************************
+    PLEASE INPUT CODE DURING 15s
+    ****************************
+    `);
 
-    console.log('click CONTINUE WITH');
+    console.log('Waiting for 15s...');
+    await page.waitForTimeout(15000)
+
+    console.log('click GET STARTED') 
     await Promise.all([
-        await page.click('body > div.s-container.s-gridMaxXs24.layouts-shared-offering_flow > div > div > div.s-grid-colSm16 > div > div > div.js-existing_entity > a'),
-        page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
-    ]); 
+        page.click('body > div.s-container.s-gridMaxXs24.layouts-shared-offering_flow > div > div > div.s-grid-colSm16 > div > div > a'),
+        page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+    ])  
+
+    console.log('click CONTINUE WITH') 
+    await Promise.all([
+        page.click('body > div.s-container.s-gridMaxXs24.layouts-shared-offering_flow > div > div > div.s-grid-colSm16 > div > div > div.js-existing_entity > a'),
+        page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+    ])  
 
     console.log('change SELECT')
     const selectElem = await page.$('#forms_offerings_participants_residence_residence_country')
     await selectElem.type('RU')
     await page.click('#new_forms_offerings_participants_residence > div:nth-child(7) > div > label')
 
-    console.log('click CONTINUE');
+    console.log('click CONTINUE') 
     await Promise.all([
         page.click('#new_forms_offerings_participants_residence > div.s-marginTop2 > a'),
         page.waitForNavigation({ waitUntil: 'domcontentloaded' })
     ])
 
-    console.log('continue is finish');
+    console.log('select ANSWERS');
+    await page.evaluate((answers) => {
+        const fields = document.querySelectorAll('.c-label.c-label--inline.s-marginLeft1');
 
+        answers.forEach( answer => {
+            [...fields].find( field => field.innerText === answer).click();
+        });
+
+    }, answers) 
+
+    console.log('FINISH');
 }
 
-start();
+start() 
